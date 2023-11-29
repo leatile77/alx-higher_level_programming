47ce6732d635603aab5385b339c931172130985b
leatile77 <moilaleatile@gmail.com>
2023-11-29 16:35:54 +0200

    print 0 - 100

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 for x in range(00, 100):
-    if x < 10:
-        print(f"0{x}", end=', ')
-        continue
-    print(f"{x}", end='\n' if x == 99 else ", ")
+    print("{:02d}".format(x), end="\n" if x == 99 else ", ")
