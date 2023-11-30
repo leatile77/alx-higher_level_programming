26bc17db1819a9055d6f95a9ac34a28ebc9d6375
leatile77 <moilaleatile@gmail.com>
2023-11-30 04:04:53 +0200

    toUpper func

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-
 def conv_upper(char):
     if ord(char) >= 97 and ord(char) <= 122:
         return (ord(char) - 32)
     else:
         return ord(char)
 
+    
 def uppercase(str):
     new_str = ""
     for letter in str:
