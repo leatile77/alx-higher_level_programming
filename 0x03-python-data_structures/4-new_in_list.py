ac8d95d0072bfcdc9da292237cab4d9dfbc45a53
leatile77 <moilaleatile@gmail.com>
2023-12-02 17:59:49 +0200

    change elem in copy of list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     new_list = my_list[:]
-    if len(my_list) <= idx and idx <= 0:
+    if idx < 0:
+        return new_list
+
+    size = len(my_list) - 1
+    if idx > size:
         return new_list
     else:
         new_list[idx] = element
