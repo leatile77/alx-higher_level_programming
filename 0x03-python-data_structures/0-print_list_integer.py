6506bebd99d56ac87b19d75ce6e8bc980dad7eac
leatile77 <moilaleatile@gmail.com>
2023-12-02 15:06:43 +0200

    RM and print int list

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+# print_list_integer - prints int items of a list
+# my_list: list to print
+
 def print_list_integer(my_list=[]):
     for i in my_list:
         print("{}".format(i))
