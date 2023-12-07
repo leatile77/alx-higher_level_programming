5ada7234269c070617e9aa971d4c3b7ebce68caf
leatile77 <moilaleatile@gmail.com>
2023-12-07 03:16:09 +0200

    convert romans to digits

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    romans = {'I': 1, 'V':5, 'X':10, 'L':50,'C':100, 'D':500}
-    digits = 0
-    prev = 0
+    if type(roman_string) == str and roman_string is not None:
+        romans = {'I': 1, 'V':5, 'X':10, 'L':50,'C':100, 'D':500}
+        digits = 0
+        prev = 0
 
-    for i in reversed(roman_string):
-        value = romans[i]
-        if value < prev:
-            digits -= value
-        else:
-            digits += value
+        for i in reversed(roman_string):
+            value = romans[i]
+            if value < prev:
+                digits -= value
+            else:
+                digits += value
 
-        prev = value
+            prev = value
 
-    return (digits)
+        return (digits)
+    else:
+        return (0)
