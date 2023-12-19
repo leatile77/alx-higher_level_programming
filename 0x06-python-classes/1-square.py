fe128e384f669aaced22d190ccfcbd18773b1d84
leatile77 <moilaleatile@gmail.com>
2023-12-19 23:39:55 +0200

    square with private size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""A class called Square"""
+
+
+class Square:
+    """Inside the square"""
+    def __init__(self, size):
+        """Initializes a square.
+        Args:
+        size (int): The size of the square
+        """
+        self.__size = size
