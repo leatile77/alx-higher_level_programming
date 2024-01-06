29ab5ddb8390dd4ccfc0017ea630e1c2e1830d13
leatile77 <moilaleatile@gmail.com>
2024-01-06 13:56:49 +0200

    locked class

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+"""A locked class."""
 class LockedClass:
+    """prevents the user from dynamically
+    creating new instance attributes.
+    """
     __slots__ = ["first_name"]
