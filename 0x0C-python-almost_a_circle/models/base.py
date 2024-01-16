85d59c02203a9c745490af6c166b129f077a2567
leatile77 <moilaleatile@gmail.com>
2024-01-16 02:22:05 +0200

    base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,25 +1,34 @@
 #!/usr/bin/python3
 
+"""Defines a base class."""
+
 import json
 class Base:
+    """Base model.
+    private class attribute:
+    __nb_objects (int): Number of instantiated bases.
+    """
     __nb_objects = 0
     def __init__(self, id=None):
-
+        """Initializes new base.
+        Args:
+        id (int): identity of new base.
+        """
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-"""
-    @staticmethod
+
+"""@staticmethod
     def to_json_string(list_dictionaries):
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
 
-        return (json.dumps(list_dictionaries))
+        return (json.dumps(list_dictionaries))"""
         
 
-    @classmethod
+""" @classmethod
     def save_to_file(cls, list_objs):
         filename = "{}.json".format(cls.__name__)
 
@@ -28,15 +37,15 @@ class Base:
                 jsonfile.write("[]")
             else:
                 dc_list = [obj.to_dictionary() for obj in list_objs]
-                jsonfile.write(Base.to_json_string(dc_list))
+                jsonfile.write(Base.to_json_string(dc_list))"""
 
-    @staticmethod
+"""@staticmethod
     def from_json_string(json_string):
         if json_string is None or json_string == "[]":
             return []
-        return json.loads(json_string)
+        return json.loads(json_string)"""
 
-    @classmethod
+""" @classmethod
     def create(cls, **dictionary):
         if dictionary:
             if cls.__name__ == "Rectangle":
@@ -44,10 +53,10 @@ class Base:
             else:
                 updated = cls(1)
             updated.update(**dictionary)
-            return updated
+            return updated"""
 
     
-    @classmethod
+""" @classmethod
     def load_from_file(cls):
         filename = "{}.json".format(cls.__name__)
         try:
