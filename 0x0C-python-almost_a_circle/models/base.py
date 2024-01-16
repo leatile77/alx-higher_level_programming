2abd04714ed3ba7297315fe5da2ab54ff7a5d3ee
leatile77 <moilaleatile@gmail.com>
2024-01-16 02:11:11 +0200

    base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,11 +5,11 @@ class Base:
     __nb_objects = 0
     def __init__(self, id=None):
 
-        if id != None:
+        if id is not None:
             self.id = id
         else:
-            type(self).__nb_objects += 1
-            self.id = type(self).__nb_objects
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
 
     @staticmethod
     def to_json_string(list_dictionaries):
