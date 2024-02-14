b53e57b221c86dc5e8f38b93848e10728ba821a8
leatile77 <moilaleatile@gmail.com>
2024-02-14 19:16:20 +0200

    Create DB, user with password and select privileges

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,8 @@
 -- Creates hbtn_0d_2 database and user_0d_2 user with select privilege and password user_0d_2_pwd
+-- Creates database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- Create user if not exists, with password
 CREATE USER IF NOT EXISTS 'user_0d_d'@'%' IDENTIFIED BY 'user_0d_2_pwd';
+-- Grant select permisiion
 GRANT SELECT ON hbtn_0d_2 TO 'user_0d_2'@'%';
 FLUSH PRIVILEGES;
