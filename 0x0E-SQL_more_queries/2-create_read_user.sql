6721a9274ddb44666aee1d4ace8f58bad0592707
leatile77 <moilaleatile@gmail.com>
2024-02-14 19:21:16 +0200

    Create DB, user with password and select privileges

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -5,3 +5,5 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS user_0d_d@localhost IDENTIFIED BY 'user_0d_2_pwd';
 -- Grant SELECT permisiion
 GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
+-- Apply changes
+FLUSH PRIVILEGES;
